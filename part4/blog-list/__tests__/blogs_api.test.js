6568ad2f6805b96dc7dbef7defdcf9ac0d245330
Numbers00 <kmaquino@up.edu.ph>
2022-07-24 14:35:26 +0800

    uploaded for submission up to exer 4.14

--- part4/blog-list/__tests__/blogs_api.test.js
+++ part4/blog-list/__tests__/blogs_api.test.js
@@ -14,73 +14,112 @@ beforeEach(async () => {
   await Promise.all(promiseArr);
 });
 
-//remove long timeout if testing performance/speed
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-}, 100000);
-
-test('the correct amount of blog posts are returned', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
-}, 100000);
-
-test('HTTP POST successfully creates a new blog post', async () => {
-  const newBlog = {
-    title: 'new blog',
-    author: 'new author',
-    url: 'http://newsite.com',
-    likes: 0
-  };
-
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+//remove long timeouts if testing performance/speed
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  }, 100000);
   
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-  expect(blogsAtEnd).toContainEqual(response.body);
-}, 100000);
+  test('the correct amount of blog posts are returned', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  }, 100000);
 
-test('HTTP POST requests for blogs that do not contain "likes" will have it default to 0', async () => {
-  const newBlog = {
-    title: 'new blog',
-    author: 'new author',
-    url: 'http://newsite.com'
-  };
+  test('the unique identifier of the returned blogs is "id" not "_id"', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body[0].id).toBeDefined();
+  }, 100000);
+});
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+describe('addition of a blog', () => {
+  test('HTTP POST successfully creates a new blog post', async () => {
+    const newBlog = {
+      title: 'new blog',
+      author: 'new author',
+      url: 'http://newsite.com',
+      likes: 0
+    };
+  
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    expect(blogsAtEnd).toContainEqual(response.body);
+  }, 100000);
+  
+  test('HTTP POST requests for blogs that do not contain "likes" will have it default to 0', async () => {
+    const newBlog = {
+      title: 'new blog',
+      author: 'new author',
+      url: 'http://newsite.com'
+    };
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+  
+    const blogsAtEnd = await helper.blogsInDb();
+    const latestBlog = blogsAtEnd[blogsAtEnd.length-1];
+    expect(latestBlog.likes).toBe(0);
+  }, 100000);
+  
+  test('HTTP POST requests for blogs that do not contain "title" or "url" will return status code 400', async () => {
+    const newBlog = {
+      url: 'http://newsite.com',
+      likes: 4
+    };
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
+  }, 100000);
+});
 
-  const blogsAtEnd = await helper.blogsInDb();
-  const latestBlog = blogsAtEnd[blogsAtEnd.length-1];
-  expect(latestBlog.likes).toBe(0);
-}, 100000);
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-test('HTTP POST requests for blogs that do not contain "title" or "url" will return status code 400', async () => {
-  const newBlog = {
-    url: 'http://newsite.com',
-    likes: 4
-  };
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).not.toContainEqual(blogToDelete);
+  }, 100000);
+});
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400);
-}, 100000);
+describe('updating a blog', () => {
+  test('succeeds and returns status code 200 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+    const updatedBlog = {
+      'title': blogToUpdate.title,
+      'author': blogToUpdate.author,
+      'url': blogToUpdate.url,
+      'likes': blogToUpdate.likes + 1
+    };
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(updatedBlog.likes).toBe(blogsAtEnd.find(blog => blog.id === blogToUpdate.id).likes);
+  }, 100000);
+});
 
 afterAll(() => {
   mongoose.connection.close();
-});
-
-test('the unique identifier of the returned blogs is "id" not "_id"', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body[0].id).toBeDefined();
 });
--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -11,10 +11,35 @@ blogsRouter.post('/', async (request, response) => {
   if (!('title' in body) || !('url' in body)) {
     return response.status(400).end();
   }
-  
+
   const blog = new Blog(body);
   const result = await blog.save();
   response.status(201).json(result);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body;
+  if (!('title' in body) || !('url' in body)) {
+    return response.status(400).end();
+  }
+
+  const blog = {
+    'title': body.title,
+    'author': body.author,
+    'url': body.url,
+    'likes': body.likes
+  };
+  await Blog.findByIdAndUpdate(
+    request.params.id,
+    blog,
+    { new: true, runValidators: true, context: 'query' }
+  );
+  response.status(200).end();
 });
 
 module.exports = blogsRouter;
