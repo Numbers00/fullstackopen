691e56d3cd2a9e8f5d55aa387d6b18952ee0412e
Numbers00 <kmaquino@up.edu.ph>
2023-07-12 10:47:20 +0800

    Upload for submission up to exer 5.19

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -33,4 +33,33 @@ describe('Blog app', function() {
       cy.get('Test User logged in').should('not.exist')
     })
   })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'TestUser', password: 'TestPassword' })
+    })
+
+    it('A blog can be created', function() {
+      cy.request(Cypress.env('BACKEND') + '/blogs').its('body').should('have.length', 0)
+
+      cy.contains('Test Title Test Author').should('not.exist')
+
+      cy.contains('Create Blog').click()
+
+      cy.get('input#titleInput').type('Test Title')
+      cy.get('input#authorInput').type('Test Author')
+      cy.get('input#urlInput').type('http://test.com')
+
+      cy.contains(/^Create$/).click()
+
+      cy.contains('Test Title Test Author')
+      cy
+        .request(Cypress.env('BACKEND') + '/blogs')
+        .its('body')
+        .should('have.length', 1)
+        .and((blogs) => {
+          expect(blogs[0]).to.deep.include({ title: 'Test Title', author: 'Test Author', url: 'http://test.com' })
+        })
+    })
+  })
 })
