bfc0439ed7a2e14bb31ffec3660bbc455287ab7d
Numbers00 <kmaquino@up.edu.ph>
2023-07-19 18:58:28 +0800

    Upload for submission up to exer 6.21

--- part6/query-anecdotes-main/src/components/AnecdoteForm.js
+++ part6/query-anecdotes-main/src/components/AnecdoteForm.js
@@ -1,11 +1,29 @@
+import { useMutation, useQueryClient } from 'react-query';
+
+import { createAnecdote } from '../requests';
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes');
+    },
+  });
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    if (content.length < 5) return;
+    
+    event.target.anecdote.value = '';
+
+    const anecdote = {
+      content: content,
+      id: (100000 * Math.random()).toFixed(0),
+      votes: 0
+    };
+    newAnecdoteMutation.mutate(anecdote);
+  }
 
   return (
     <div>
@@ -15,7 +33,7 @@ const AnecdoteForm = () => {
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
--- part6/query-anecdotes-main/src/requests.js
+++ part6/query-anecdotes-main/src/requests.js
@@ -4,3 +4,6 @@ const baseUrl = 'http://localhost:3001';
 
 export const getAnecdotes = () =>
   axios.get(`${baseUrl}/anecdotes`).then((res) => res.data);
+
+export const createAnecdote = anecdote =>
+  axios.post(`${baseUrl}/anecdotes`, anecdote).then((res) => res.data);
