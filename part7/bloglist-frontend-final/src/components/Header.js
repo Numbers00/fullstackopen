b5737065496ca3e9ed968f222c01033dde2b3c06
Numbers00 <kmaquino@up.edu.ph>
2023-07-22 16:11:54 +0800

    Upload for submission up to exer 7.17

--- part7/bloglist-frontend-final/src/components/Header.js
+++ part7/bloglist-frontend-final/src/components/Header.js
@@ -5,20 +5,7 @@ import { useNotificationValue } from '../contexts/NotificationContext';
 
 
 const TopNav = () => {
-  return (
-    <div style={{ display: 'flex' }}>
-      <Link to='/blogs' style={{ marginRight: 8 }}>blogs</Link>
-      <Link to='/users' style={{ marginRight: 8 }}>users</Link>
-    </div>
-  );
-};
-
-
-const Header = () => {
   const { user } = useAuthValue();
-  const notification = useNotificationValue();
-  const errorMessage = notification.type === 'error' ? notification.message : null;
-  const successMessage = notification.type === 'success' ? notification.message : null;
 
   const logout = useLogout();
   const handleLogout = () => {
@@ -26,12 +13,27 @@ const Header = () => {
   };
 
   return (
+    <div style={{ display: 'flex' }}>
+      <Link to='/blogs' style={{ marginRight: 8 }}>blogs</Link>
+      <Link to='/users' style={{ marginRight: 8 }}>users</Link>
+      <span>
+        Logged in as {user.username} &nbsp;
+        <button type='button' onClick={handleLogout}>Logout</button>
+      </span>
+    </div>
+  );
+};
+
+
+const Header = () => {
+  const notification = useNotificationValue();
+  const errorMessage = notification.type === 'error' ? notification.message : null;
+  const successMessage = notification.type === 'success' ? notification.message : null;
+
+  return (
     <div>
       <TopNav />
       <h2>blogs</h2>
-      <p>
-        {user.name} logged in <button type='button' onClick={handleLogout}>Logout</button>
-      </p>
       { successMessage && <span style={{ color: 'green' }}>{successMessage}</span> }
       { errorMessage && <span style={{ color: 'red' }}>{errorMessage}</span> }
     </div>
