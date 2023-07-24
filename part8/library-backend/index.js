5c37d301997ab192c9a3676871f72feb255b3e86
Numbers00 <kmaquino@up.edu.ph>
2023-07-24 11:50:42 +0800

    Upload for submission up to exer 8.13

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -3,31 +3,31 @@ const { startStandaloneServer } = require('@apollo/server/standalone');
 const { GraphQLError } = require('graphql');
 const { v1: uuid } = require('uuid');
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
+// let authors = [
+//   {
+//     name: 'Robert Martin',
+//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+//     born: 1952,
+//   },
+//   {
+//     name: 'Martin Fowler',
+//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+//     born: 1963
+//   },
+//   {
+//     name: 'Fyodor Dostoevsky',
+//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+//     born: 1821
+//   },
+//   { 
+//     name: 'Joshua Kerievsky', // birthyear not known
+//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+//   },
+//   { 
+//     name: 'Sandi Metz', // birthyear not known
+//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+//   },
+// ];
 
 /*
  * Suomi:
@@ -43,67 +43,78 @@ let authors = [
  * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
 */
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-];
+// let books = [
+//   {
+//     title: 'Clean Code',
+//     published: 2008,
+//     author: 'Robert Martin',
+//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+//     genres: ['refactoring']
+//   },
+//   {
+//     title: 'Agile software development',
+//     published: 2002,
+//     author: 'Robert Martin',
+//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+//     genres: ['agile', 'patterns', 'design']
+//   },
+//   {
+//     title: 'Refactoring, edition 2',
+//     published: 2018,
+//     author: 'Martin Fowler',
+//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+//     genres: ['refactoring']
+//   },
+//   {
+//     title: 'Refactoring to patterns',
+//     published: 2008,
+//     author: 'Joshua Kerievsky',
+//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+//     genres: ['refactoring', 'patterns']
+//   },  
+//   {
+//     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+//     published: 2012,
+//     author: 'Sandi Metz',
+//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+//     genres: ['refactoring', 'design']
+//   },
+//   {
+//     title: 'Crime and punishment',
+//     published: 1866,
+//     author: 'Fyodor Dostoevsky',
+//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+//     genres: ['classic', 'crime']
+//   },
+//   {
+//     title: 'The Demon ',
+//     published: 1872,
+//     author: 'Fyodor Dostoevsky',
+//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+//     genres: ['classic', 'revolution']
+//   },
+// ];
 
-/*
-  you can remove the placeholder query once your first own has been implemented 
-*/
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const Author = require('./models/author');
+const Book = require('./models/book');
+
+require('dotenv').config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+console.log('connecting to', MONGODB_URI);
+
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message));
 
 const typeDefs = `
   type Book {
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     id: ID!
     genres: [String!]!
   }
@@ -138,49 +149,81 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    allBooks: (root, args) => {
-      if (!args.author && !args.genre)
-        return books;
-      else if (args.author && !args.genre)
-        return books.filter(b => b.author === args.author);
-      else if (!args.author && args.genre)
-        return books.filter(b => b.genres.includes(args.genre));
-      else
-        return books.filter(b => b.author === args.author && b.genres.includes(args.genre));
-    },
-    authorCount: () => authors.length,
-    allAuthors: () => authors,
+    bookCount: async () => Book.collection.countDocuments(),
+    // allBooks: (root, args) => {
+    //   if (!args.author && !args.genre)
+    //     return books;
+    //   else if (args.author && !args.genre)
+    //     return books.filter(b => b.author === args.author);
+    //   else if (!args.author && args.genre)
+    //     return books.filter(b => b.genres.includes(args.genre));
+    //   else
+    //     return books.filter(b => b.author === args.author && b.genres.includes(args.genre));
+    // },
+    authorCount: async () => Author.collection.countDocuments(),
+    allAuthors: async () => Author.find({}),
   },
   Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args, id: uuid() };
-      books = books.concat(book);
-      if (!authors.find(a => a.name === args.author))
-        authors = authors.concat({ name: args.author, id: uuid() });
-      
-      return book;
+    addBook: async (root, args) => {
+      try {
+        let author;
+        try {
+          author = await (async () => {
+            const author = await Author.findOne({ name: args.author });
+            console.log('author', author);
+            return author ? author : await (new Author({ name: args.author })).save();
+          })();
+        } catch (error) {
+          console.log(error);
+          throw new GraphQLError(`Error adding author: ${err.message}`, {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.author,
+              error
+            }
+          });
+        }
+        
+        const book = new Book({ ...args, author: author._id.toString() });
+        try {
+          await book.save();
+        } catch (error) {
+          console.log(error);
+          throw new GraphQLError(`Error adding book: ${error.message}`, {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: [args.title, args.published, args.genres],
+              error
+            }
+          });
+        }
+
+        return book;
+      } catch (error) {
+        console.log(error);
+        return error.message;
+      }
     },
-    editAuthor: (root, args) => {
-      if (args.setBornTo >= (new Date()).getFullYear())
-        throw new GraphQLError('Invalid birth year', {
-          extensions: {
-            code: 'BAD_USER_INPUT',
-            invalidArgs: args.setBornTo
-          }
-        });
+    // editAuthor: (root, args) => {
+    //   if (args.setBornTo >= (new Date()).getFullYear())
+    //     throw new GraphQLError('Invalid birth year', {
+    //       extensions: {
+    //         code: 'BAD_USER_INPUT',
+    //         invalidArgs: args.setBornTo
+    //       }
+    //     });
 
-      const author = authors.find(a => a.name === args.name);
-      if (!author) return null;
+    //   const author = authors.find(a => a.name === args.name);
+    //   if (!author) return null;
 
-      const updatedAuthor = { ...author, born: args.setBornTo };
-      authors = authors.map(a => a.name === args.name ? updatedAuthor : a);
-      return updatedAuthor;
-    }
+    //   const updatedAuthor = { ...author, born: args.setBornTo };
+    //   authors = authors.map(a => a.name === args.name ? updatedAuthor : a);
+    //   return updatedAuthor;
+    // }
   },
-  Author: {
-    bookCount: (root) => books.filter(b => b.author === root.name).length
-  }
+  // Author: {
+  //   bookCount: (root) => books.filter(b => b.author === root.name).length
+  // }
 };
 
 const server = new ApolloServer({
