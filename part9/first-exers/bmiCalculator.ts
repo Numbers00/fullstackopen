5f74aae4366bd8f1566dc9031a6ae497629f8cfe
Numbers00 <kmaquino@up.edu.ph>
2023-07-27 20:53:46 +0800

    Upload for submission up to exer 9.1

---
+++ part9/first-exers/bmiCalculator.ts
@@ -0,0 +1,9 @@
+// height in cm, weight in kg
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi: number = weight / ((height / 100) ** 2);
+  if (bmi < 18.5) return 'Underweight';
+  else if (18.5 <= bmi && bmi < 25) return 'Normal (healthy weight)';
+  else if (25 <= bmi && bmi < 30) return 'Overweight';
+  else return 'Obese';
+};
+console.log(calculateBmi(180, 74));
