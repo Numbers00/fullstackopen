fdfefe1414dde027b315fe8861f95b19186ae8d8
Numbers00 <kmaquino@up.edu.ph>
2023-07-29 10:57:41 +0800

    Upload for submission up to exer 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,8 +1,12 @@
+import cors from 'cors';
 import express from 'express';
 
 
 const app = express();
 app.use(express.json());
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
