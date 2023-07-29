bf30696346cc5632d2d74a5a08d0de8b6b58614c
Numbers00 <kmaquino@up.edu.ph>
2023-07-29 17:06:49 +0800

    Upload for submission up to exer 9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 
+import diagnosesRouter from './routes/diagnoses';
+
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,8 @@ app.use(cors());
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
