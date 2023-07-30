17b68f3c2decd036f434a72a796f20aa37c6e128
Numbers00 <kmaquino@up.edu.ph>
2023-07-30 16:39:36 +0800

    Upload for submission up to exer 9.19

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -60,19 +60,39 @@ function App() {
       <form id='addDiaryEntryForm' onSubmit={addDiaryEntry}>
         <div>
           <label htmlFor='dateInput'>Date</label>
-          <input type='string' id='dateInput' value={newDiaryEntry.date} onChange={e => setNewDiaryEntry({ ...newDiaryEntry, date: e.target.value })} />
+          <input type='date' id='dateInput' value={newDiaryEntry.date} onChange={e => setNewDiaryEntry({ ...newDiaryEntry, date: e.target.value })} />
         </div>
         <div>
-          <label htmlFor='weatherInput'>Weather</label>
-          <select id='weatherInput' value={newDiaryEntry.weather} onChange={e => setNewDiaryEntry({ ...newDiaryEntry, weather: e.target.value as Weather })}>
-            {Object.values(Weather).map(w => <option key={w} value={w}>{w}</option>)}
-          </select>
+          <span style={{ marginRight: 8 }}>Weather</span>
+          {Object.values(Weather).map(w => (
+            <>
+              <label key={w + 'Label'} htmlFor={w + 'WeatherRadio'}>{ w }</label>
+              <input
+                type='radio'
+                id={w + 'WeatherRadio'}
+                key={w + 'Radio'}
+                name='weatherRadio'
+                onChange={() => setNewDiaryEntry({ ...newDiaryEntry, weather: w })}
+                checked={newDiaryEntry.weather === w}
+              />
+            </>
+          ))}
         </div>
         <div>
-          <label htmlFor='visibilityInput'>Visibility</label>
-          <select id='visibilityInput' value={newDiaryEntry.visibility} onChange={e => setNewDiaryEntry({ ...newDiaryEntry, visibility: e.target.value as Visibility })}>
-            {Object.values(Visibility).map(v => <option key={v} value={v}>{v}</option>)}
-          </select>
+          <span style={{ marginRight: 8 }}>Visibility</span>
+          {Object.values(Visibility).map(v => (
+            <>
+              <label key={v + 'Label'} htmlFor={v + 'VisibilityRadio'}>{ v }</label>
+              <input
+                type='radio'
+                id={v + 'VisibilityRadio'}
+                key={v + 'Radio'}
+                name='visibilityRadio'
+                onChange={() => setNewDiaryEntry({ ...newDiaryEntry, visibility: v })}
+                checked={newDiaryEntry.visibility === v}
+              />
+            </>
+          ))}
         </div>
         <div>
           <label htmlFor='commentInput'>Comment</label>
